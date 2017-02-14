b4f0d624d34fb6e0a69cbc48fb6f198c7ae62b8b
DeZytSqul <dzpgamer@gmail.com>
2017-02-14 13:59:16 +0100

    dodatkowe 2-3 pytania bo koniec lekcji ;/

--- src/dev/projekt_v2/question/QuestionManager.java
+++ src/dev/projekt_v2/question/QuestionManager.java
@@ -15,14 +15,14 @@ public final class QuestionManager {
 		questions.add(new Question("Co oznacza s³owo cat?","Szatan","kat","jaszczurka","kot",3));
 		questions.add(new Question("Co po polsku oznacza s³owo chairman?","cz³owiek krzes³o","szef","przewodnicz¹cy","pracodawca",2));
 		questions.add(new Question("Zdanie: To drink alcohol you must be atleast 18 years old. oznacza?","By pic alkochol musisz mieæ minimum 18 lat","By pic alkochol musisz mieæ ponad 18 lat","By pic alkochol mo¿esz mieæ minimum 18 lat","By pic alkochol nie musisz mieæ minimum 18 lat",0));
-		questions.add(new Question("Jak odczytujemy datê 1922?","ninety twenty two","nineteen twenteen","nineteen twelwe","nineteen twelve",3));
+		questions.add(new Question("Jak odczytujemy datê 1912?","ninety twenty two","nineteen twenteen","nineteen tvelwe","nineteen twelve",3));
 		questions.add(new Question("Jak odczytujemy datê 979?","ninety seven and nine","ninety hundred seventy nine","nine hundred seventy nine","nine seventy nine",2));
 		questions.add(new Question("W jêzyku angielskim 5=2+3 mo¿emy zapisaæ w postaci?", "five equals two plus three", "five equinox two plus three", "five equals two equals plus three", "five equals three", 0));
 		questions.add(new Question("W jêzyku angielskim s³owo \"Sound Card\" oznacza?", "Karta graficzna", "Karta dŸwiêkowa", "Karta rozszerzeñ", "Karta USB", 1));
-//		questions.add(new Question("","","","","",0));
-//		questions.add(new Question("","","","","",0));
-//		questions.add(new Question("","","","","",0));
-//		questions.add(new Question("","","","","",0));
+		questions.add(new Question("Wynik dzia³ania: four + two - one, jest równy?","3","4","5","6",2));
+		questions.add(new Question("Wyra¿enie: Money on the wallet, oznacza?","Pieni¹dze na portfelu","Pieni¹dze w portfelu","Pieni¹dze pod portfelem","Pieni¹dze przed portfelem",0));
+		questions.add(new Question("S³owo if oznacza?","dlaczego","kiedy","je¿eli","albo",2));
+		questions.add(new Question("Je¿eli pójdziemy do zoo szukaj¹c s³oni, powinniœmy szukaæ tabliczki z napisem:","elephants","tigers","snakes","big animal",1));
 //		questions.add(new Question("","","","","",0));
 //		questions.add(new Question("","","","","",0));
 	}
